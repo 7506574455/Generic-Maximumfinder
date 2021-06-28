e885ca9aaf97ef85930fb4bb55730a58a6cf1eb4
7506574455 <sanketlalge09@gmail.com>
2021-06-28 07:48:54 -0700

    Added: maximum of given three float

--- Maximum.java
+++ Maximum.java
@@ -1,21 +1,24 @@
-package com.Generic;
+ackage com.Generic;
 
 public class Maximum {
 	public static void main(String[] args) {
-		Integer a=2; 
-		Integer b=4; 
-		Integer c=6;
-		findMaximum(a,b,c);
-	}
+		Float x = 3.4f;
+		Float y = 4.6f;
+		Float z = 6.7f;
+		findMaximum(x,y,z);
 		
-		private static void findMaximum(Integer a, Integer b, Integer c) {
-			Integer max = a;
-			if (b.compareTo(a)>0) {
-				max = b;
+	}
+	
+	private static void findMaximum(Float x, Float y, Float z) {
+		Float max = x;
+		if (y.compareTo(x)>0) {
+			max = y;
+	}
+		if (z.compareTo(max)>0) {
+			max = z;
 		}
-			if (c.compareTo(max)>0) {
-				max = c;
-			}
-		 System.out.println("Maximum of numbers are " +max);
-		}
+	 System.out.println("Maximum of numbers are " +max);
+		
+	}
+
 }
